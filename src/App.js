74b99db09ac827b85c71af4764cf39c38e3a5f4e
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-06-04 13:03:10 -0400

    added particles to the background!

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import './App.css';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
+import Particles from 'react-particles-js';
+import particleFile from './particles.json';
+
 
 function App() {
   return (
     <div className="demo-big-content">
-    <Layout>
+      <Layout>
         <Header title="Title" scroll>
-            <Navigation>
-                <a href="/resume">Resume</a>
-                <a href="/projects">Projects</a>
-                <a href="/contact">Contact</a>
-                <a href="/aboutme">About Me</a>
-            </Navigation>
+          <Navigation>
+            <a href="/resume">Resume</a>
+            <a href="/projects">Projects</a>
+            <a href="/contact">Contact</a>
+            <a href="/aboutme">About Me</a>
+          </Navigation>
         </Header>
         <Drawer title="Title">
-            <Navigation>
-                <a href="/resume">Resume</a>
-                <a href="/projects">Projects</a>
-                <a href="/contact">Contact</a>
-                <a href="/aboutme">About Me</a>
-            </Navigation>
+          <Navigation>
+            <a href="/resume">Resume</a>
+            <a href="/projects">Projects</a>
+            <a href="/contact">Contact</a>
+            <a href="/aboutme">About Me</a>
+          </Navigation>
         </Drawer>
         <Content>
-            <div className="page-content" />
-            <Main/>
+          <div className="page-content" />
+          <Main />
+          <Particles
+            params={particleFile}
+          />
         </Content>
-    </Layout>
-</div>
+      </Layout>
+
+    </div>
   );
 }
 
