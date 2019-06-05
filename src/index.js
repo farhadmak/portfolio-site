a38a1a7a530803ce85e8e576a4b3acb8814042b1
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-06-05 10:09:18 -0400

    changing back to hashrouter

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { HashHistory } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <HashHistory>
+    <HashRouter>
     <App />
-    </HashHistory>
+    </HashRouter>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
