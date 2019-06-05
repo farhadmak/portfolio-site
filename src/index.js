474fa200726045023bc845c0d72fd2849939ef5c
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-06-05 12:42:14 -0400

    testing to see if this works on netlify

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <HashRouter basename={'process.env.PUBLIC_URL'}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-    </HashRouter>
+    </BrowserRouter>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
