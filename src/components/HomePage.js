8583b0982923a9bff9940a8d5e33c1cb4e50a13a
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-06-06 01:08:41 -0400

    setting up backgrounds and colour themes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import profilePic from '../images/profile_pic.png'
 
 class HomePage extends Component {
-    render() {
-        return (
-            <div>
-                <img className="profile-pic" src={profilePic} alt="Farhad"/>
-                <div className="text-container">
-                    <div className="offset center">I'm Farhad Makiabady. This is a test message tes</div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <img className="profile-pic" src={profilePic} alt="Farhad" />
+        <div className="text-container">
+          <div className="offset center">I'm Farhad Makiabady. This is a test message tes</div>
+        </div>
+      </div>
+    )
+  }
 }
 export default HomePage;
