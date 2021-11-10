9afacbfc411deb125f0d3fb4258b1a6c27176f3d
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2021-11-09 17:18:08 -0700

    Updating portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
       <div>
         <div className="text-container">
           <h1>Contact me</h1>
-          <p>Coming soon!</p>
+          <p>Coming soon! Send me an email at makiabad@ualberta.ca!</p>
         </div>
       </div>
     )
